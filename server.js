aa4fcf55582a107ee4665e2664ec33288191a3f2
devika-cashew <devika@cashew.ai>
2023-05-02 10:22:07 +0200

    Port

--- server.js
+++ server.js
@@ -20,7 +20,7 @@
 require('dotenv').config({silent: true});
 
 var server = require('./app');
-var port = process.env.PORT || 3003;
+var port = process.env.PORT || 8080;
 
 server.listen(port, function() {
   console.log('Server running on port: %d', port);
